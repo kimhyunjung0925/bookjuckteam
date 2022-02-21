94539522c714529c051e3374ceb77107f2b045dc
apf_temp_admin <khjung0925@naver.com>
2022-02-21 15:57:22 +0900

    이메일인증번호 발송 기능 구현 완료

--- src/main/java/com/project/bookjuck/BookjuckApplication.java
+++ src/main/java/com/project/bookjuck/BookjuckApplication.java
@@ -1,14 +1,26 @@
 package com.project.bookjuck;
 
+import com.project.bookjuck.email.EmailSendService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class BookjuckApplication {
 
+    @Autowired
+    private EmailSendService service;
+
     public static void main(String[] args) {
         SpringApplication.run(BookjuckApplication.class, args);
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void triggerMail() {
+        service.sendSimpleEmail("khjung0925@naver.com"); //받는사람 입력
     }
 
 }
