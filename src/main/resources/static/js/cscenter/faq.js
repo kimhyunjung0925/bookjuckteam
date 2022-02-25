b425ea7410a27c67d685da781b9349b9676d5cd1
apf_temp_admin <khjung0925@naver.com>
2022-02-25 12:52:41 +0900

    cscenter 대대적 공사 중

--- src/main/resources/static/js/cscenter/faq.js
+++ src/main/resources/static/js/cscenter/faq.js
@@ -1,8 +1,5 @@
 (function (){
     'use strict';
-    //글자 수 제한하는 함
-
-    const faqElem = document.querySelector('.faq');
 
     //faq리스트 정보 가져오기
     const getFaqList = () => {
@@ -13,53 +10,90 @@
 
     //faq레코드
     const makeFaqRecordList = list => {
-        const faqListElem = faqElem.querySelector('.faqboard');
-
+        const faqMenuElem = document.querySelector('#faq_menu');
+        const faqtableElem = document.querySelector('#faqboardtable');
         //각 분류마다 엘레먼트 만들어줌
-        const cate1 = faqListElem.querySelector('.faqcategory_1');
-        const cate2 = faqListElem.querySelector('.faqcategory_2');
-        const cate3 = faqListElem.querySelector('.faqcategory_3');
-        const cate4 = faqListElem.querySelector('.faqcategory_4');
+        const cate1 = faqMenuElem.querySelector('.faqcategory_1');
+        const cate2 = faqMenuElem.querySelector('.faqcategory_2');
+        const cate3 = faqMenuElem.querySelector('.faqcategory_3');
+        const cate4 = faqMenuElem.querySelector('.faqcategory_4');
 
-        list.forEach(item => {
-            const divElem = document.createElement('div');
-            switch(item.faq_cate){
-                case 1:
-                    cate1.appendChild(divElem);
-                    categoryrecord(item,divElem);
-                break;
-                case 2:
-                    cate2.appendChild(divElem);
-                    categoryrecord(item,divElem);
-                    break;
-                case 3:
-                    cate3.appendChild(divElem);
-                    categoryrecord(item, divElem);
-                    break;
-                case 4:
-                    cate4.appendChild(divElem);
-                    categoryrecord(item, divElem);
-                    break;
-            }
+        cate1.addEventListener("click", (e) => {
 
-            const titleElem = divElem.querySelector('#faq_title');
-            titleElem.addEventListener('click', e => {
-                location.href = `/cscenter/faq/faqdetail?ifaq=${item.ifaq}`;
-            });
         })
+
+
     }
 
     //카테고리안에 아이템마다 레코드 만들어주는 함수
-    const categoryrecord = (item, divElem) => {
-        divElem.className = "px-2 col  align-items-start";
-        divElem.innerHTML = `
+    const categoryrecord = (item, faqtableElem) => {
+        faqtableElem.innerHTML = `
                 <h5 id="faq_title" class="fw-bold pointer">${item.faq_title}</h5>
                 <pre>${item.faq_ctnt}</pre>
                 <hr>
-               
-            `;
+        `;
+
     }
 
     getFaqList();
 
 })();
+
+
+
+
+
+//
+// (function (){
+//     'use strict';
+//
+//     const cscenterElem = document.querySelector('.cscenter');
+//     const faqElem = cscenterElem.querySelector('.faq');
+//
+//     //faq리스트 정보 가져오기
+//     const getFaqList = () => {
+//         myFetch.get(`/ajax/cscenter/faq`, list=>{
+//             makeFaqRecordList(list);
+//         });
+//     }
+//
+//     //faq레코드
+//     const makeFaqRecordList = list => {
+//         const faqListElem = faqElem.querySelector('.faqboard');
+//
+//         for(var i = 0; i<9; i++){
+//             const divElem = document.createElement('div');
+//             faqListElem.appendChild(divElem);
+//
+//             divElem.className = " px-2 col  align-items-start";
+//             divElem.innerHTML = `
+//                 <strong class="head back_color_grey2">${list[i].faq_title}</strong>
+//                 <div class="body m-t-10" style="display: none">
+//                     <pre>${list[i].faq_ctnt}</pre>
+//                     <div class="color_grey font_size_small">원하는 답변을 얻지 못하셨나요 ? [1:1상담]을 이용해주세요.</div>
+//                 </div>
+//
+//                 <hr>
+//             `;
+//
+//         }
+//         const head = document.getElementsByClassName("head");
+//
+//         for (var j = 0; j < head.length; j++) {
+//             head[j].addEventListener("click", function() {//클릭이벤트를 추가한다.
+//                 this.classList.toggle("active");// 클래스를 추가하거나 삭제함.
+//                 var body = this.nextElementSibling; //현재 아코디언의 다음노트를 가져온다.
+//
+//                 if (body.style.display === "none") { //출력모드가 블럭인지 none인지 체크한다.
+//                     body.style.display = "block"; }
+//                 else {
+//                     body.style.display = "none";
+//                 }
+//             });
+//         }
+//
+//
+//     }
+//     getFaqList();
+//
+// })();
