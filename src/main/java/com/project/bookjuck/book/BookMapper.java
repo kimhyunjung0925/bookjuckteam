7503bfc60d4b6b6cbb771c2d5d2766f4425d291a
KownJY <eungi960@gmail.com>
2022-02-26 13:04:37 +0900

    리스트 뿌리기 완료. 최신순 정렬.

--- src/main/java/com/project/bookjuck/book/BookMapper.java
+++ src/main/java/com/project/bookjuck/book/BookMapper.java
@@ -1,8 +1,12 @@
 package com.project.bookjuck.book;
 
+import com.project.bookjuck.book.model.BookDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BookMapper {
-
+    int insBookApi(BookDto dto);
+    List<BookDto> sel(BookDto dto);
 }
