da3f426eede73456109eff9d015d8e8a4bf44314
apf_temp_admin <khjung0925@naver.com>
2022-03-03 20:15:11 +0900

    카테고리 수정, 베스트 js 하는 중

--- src/main/resources/static/js/book/best.js
+++ src/main/resources/static/js/book/best.js
@@ -1,8 +1,9 @@
-function bookSort(){
-    // const form = document.querySelector('.sortType');
-    // const a1= form.querySelector('#a1');
-    // form.addEventListener('submit', bookSort);
-    document.getElementById('.sortType').submit();
-}
+// function bookSort(){
+//     // const form = document.querySelector('.sortType');
+//     // const a1= form.querySelector('#a1');
+//     // form.addEventListener('submit', bookSort);
+//     document.getElementById('.sortType').submit();
+// }
+
 
 
