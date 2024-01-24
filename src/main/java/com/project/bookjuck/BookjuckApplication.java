fdb4ce3ff1ab684b6fb575464a9650323a3e190e
apf_temp_admin <khjung0925@naver.com>
2024-01-24 14:44:51 +0900

    시작 경로 수정 중..4

--- src/main/java/com/project/bookjuck/BookjuckApplication.java
+++ src/main/java/com/project/bookjuck/BookjuckApplication.java
@@ -5,14 +5,17 @@ import com.project.bookjuck.user.model.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BookjuckApplication {
 
     @Autowired
     private EmailSendService service;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(BookjuckApplication.class, args);
