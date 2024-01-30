66953adac8c1e2f13eb447958f7e3a83cda316fa
apf_temp_admin <khjung0925@naver.com>
2024-01-30 15:11:43 +0900

    cloud에서 실행시 문제있어서 수정중2 "/"실행시 "/main"했는데 왜..

--- src/main/java/com/project/bookjuck/BookjuckApplication.java
+++ src/main/java/com/project/bookjuck/BookjuckApplication.java
@@ -24,14 +24,15 @@ public class BookjuckApplication {
         SpringApplication.run(BookjuckApplication.class, args);
     }
 
-//    @Controller
-//    public static class MainRedirectController {
-//
-//        @GetMapping("/")
-//        public String redirectToMain() {
-//            return "/main";
-//        }
-//    }
+    @Controller
+    public static class MainRedirectController {
+
+        @GetMapping("/")
+        public String redirectToMain() {
+            return "/main";
+        }
+    }
+
 //    @Configuration
 //    public static class ApplicationReadyListener {
 //
--- src/main/java/com/project/bookjuck/MvcConfig.java
+++ src/main/java/com/project/bookjuck/MvcConfig.java
@@ -1,15 +1,15 @@
-package com.project.bookjuck;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // "/" 경로로 접속 시 자동으로 "/main" 페이지로 이동하도록 설정
-        registry.addViewController("/").setViewName("/main");
-    }
-}
+//package com.project.bookjuck;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        // "/" 경로로 접속 시 자동으로 "/main" 페이지로 이동하도록 설정
+//        registry.addViewController("/").setViewName("/main");
+//    }
+//}
