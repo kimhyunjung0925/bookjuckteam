0f4e9e7e99aa11c7664258909cea806eb6278cf3
apf_temp_admin <khjung0925@naver.com>
2024-02-04 00:46:15 +0900

    디테일 하단 +,- 동작 완성, input작동도 됨 총 금액도 출력 완료.

--- src/main/resources/static/js/book/book.js
+++ src/main/resources/static/js/book/book.js
@@ -1,19 +1,28 @@
-let OrderQty = document.querySelector('#orderQty');
-// let OrderPlus = document.querySelector('#OrderPlus');
-// let OrderMinus = document.querySelector('#OrderMinus');
-let orderPrice = document.querySelector('#orderPrice');
+var orderQtyInput = document.querySelector('#orderQty');
+var orderPriceInput = document.querySelector('#orderPrice');
+var currentQuantity = parseInt(orderQtyInput.value); // Use orderQtyInput
+var currentPrice = parseInt(orderPriceInput.value); // Use orderPriceInput
+orderQtyInput.addEventListener('input', function () {
+    let inputValue = orderQtyInput.value; // Use orderQtyInput
+    if (inputValue <= 0) {
+        inputValue = 1;
+       orderQtyInput.value = '1';
+    }
 
-function add() {
-    OrderQty.value++;// hm 값을 1 증가
-    let sum_ = parseInt(OrderQty.value) * orderPrice.value;// hm 문자값을 정수로 변환 * sell_price	해서 금액 계산
-    document.getElementById("my_sum").innerHTML = sum_.toLocaleString('ko-KR');// id가 my_sum인 요소를 찾아 값을 위에서 계산한 sum_ 값으로 설정, 각 나라 표시방식에 맞게 설정해줌
-}
+    let totalPrice = inputValue * currentPrice;
+    document.querySelector('#totalPrice').textContent = `${totalPrice.toLocaleString()}원`;
+});
 
-function del() {
+function changeQuantity(change) {
+    let newQuantity = currentQuantity + change;
 
-    if (OrderQty.value > 1) {// hm(amounts에서 가져온 값)가 1보다 크면
-        OrderQty.value--;// hm 값 1 감소
-        var sum_ = parseInt(OrderQty.value) * orderPrice.value;
-        document.getElementById("my_sum").innerHTML = sum_.toLocaleString('ko-KR');
+    if (newQuantity >= 1) { // 수량은 최소 1이어야 합니다.
+        orderQtyInput.value = newQuantity; // Use orderQtyInput
+        let totalPrice = newQuantity * currentPrice;
+        document.querySelector('#totalPrice').textContent = `${totalPrice.toLocaleString()}원`;
+        currentQuantity = newQuantity; // Update currentQuantity
     }
 }
+// 초기 총 가격 설정
+// document.querySelector('#totalPrice').textContent = `총가격: ${totalPrice.toLocaleString()}원`;
+
